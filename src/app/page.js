94d8016fe6698dbf29338422baa9ffa9cbb5dab0
EginRyzen <muhammadaliagil@gmail.com>
2024-09-24 23:36:56 +0700

    feat:tampilan home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Navbar from "../components/navbar.js";
 
 export default function Home() {
   return (
@@ -64,7 +63,48 @@ export default function Home() {
           </div>
         </div>
 
-
+        {/* About Boot.IT */}
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
+          <div className="w-full flex justify-between items-center">
+            <div className="w-1/2 px-10">
+              <div className="text-3xl font-normal text-[#3a3f47]">Event Bootcamp</div>
+              <p className="my-5 text-start font-sm text-[#3a3f47] text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+              </p>
+            </div>
+            <div className="w-1/2 px-10 me-10">
+              <div className="w-full">
+                <img src="https://d168wuuhrgvlrj.cloudfront.net/cloudfront/templates/v1/landing/free%202_11zon.png" className="float-end" />
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex justify-between items-center">
+            <div className="w-1/2 px-10 ms-10">
+              <div className="w-full">
+                <img src="https://d168wuuhrgvlrj.cloudfront.net/cloudfront/templates/v1/landing/free%203_11zon.png" className="float-start" />
+              </div>
+            </div>
+            <div className="w-1/2 px-10">
+              <div className="text-3xl text-end font-normal text-[#3a3f47]">Event Meet Up</div>
+              <p className="my-5 text-start font-sm text-[#3a3f47] text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+          </div>
+          <div className="w-full flex justify-between items-center">
+            <div className="w-1/2 px-10">
+              <div className="text-3xl font-normal text-[#3a3f47]">Event Competition</div>
+              <p className="my-5 text-start font-sm text-[#3a3f47] text-lg">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s,
+              </p>
+            </div>
+            <div className="w-1/2 px-10 me-10">
+              <div className="w-full">
+                <img src="https://d168wuuhrgvlrj.cloudfront.net/cloudfront/templates/v1/landing/free%202_11zon.png" className="float-end" />
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
