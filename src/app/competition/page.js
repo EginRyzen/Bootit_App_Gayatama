530264b780edafd69098b0f450dc81d2c7c7c35b
kevidn <kevindzakyh25@gmail.com>
2024-09-27 19:16:25 +0700

    Add Competition File

---
+++ src/app/competition/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from "./carousel/carousel";
+
+export default function Page() {
+  return (
+    <>
+      <main>
+        <Carousel />
+      </main>
+    </>
+  )
+}
