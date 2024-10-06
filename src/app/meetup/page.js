786eaa42e75f50aa57d41737d2e7cbb68ad8ac88
EginRyzen <muhammadaliagil@gmail.com>
2024-10-06 12:15:35 +0700

    feat:pagination in meetup

--- src/app/meetup/page.js
+++ src/app/meetup/page.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Data } from './data.js'
-
+import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+import { MdOutlineKeyboardDoubleArrowLeft } from "react-icons/md";
 function Page() {
     return (
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 flex py-20 gap-10">
             <div className='w-[30%]'>
-                <div className='sticky top-32'>
+                <div className='sticky top-28'>
                     <div className='text-xl font-medium text-[#3a3f47]'>Daftar Event</div>
                     <p className='text-sm font-light text-[#3a3f47] my-5'>Kembangkan jaringan dan belajar dari developer terbaik</p>
                 </div>
@@ -78,6 +79,41 @@ function Page() {
                     ))}
 
                 </div>
+
+
+                <div className='w-full flex justify-center mt-10'>
+                    <nav aria-label="Page navigation example">
+                        <ul className="inline-flex -space-x-px text-base h-10">
+                            <li>
+                                <a href="#" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-md hover:bg-gray-100 hover:text-gray-700">
+                                    <MdOutlineKeyboardDoubleArrowLeft />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" aria-current="page" className="flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700">1</a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ">2</a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ">3</a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ">4</a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ">5</a>
+                            </li>
+                            <li>
+                                <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-md hover:bg-gray-100 hover:text-gray-700 ">
+                                    <MdOutlineKeyboardDoubleArrowRight />
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+
+
 
             </div>
         </div>
