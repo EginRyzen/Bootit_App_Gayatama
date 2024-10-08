894523bf8c0525531f0fdeaa3ea8ba8223872f2b
RN-1007 <rayhannafishdwip@gmail.com>
2024-10-08 16:44:50 +0700

    initial commit

--- src/app/competition/data.js
+++ src/app/competition/data.js
@@ -1,38 +1,74 @@
 export const competitionData = [
     {
         id: 1,
-        title: "DesignFusion: Innovate the Future of User Experience 1",
-        field: "UI/UX Designer",
-        dateStart: "01/12/2024",
-        dateEnd: "30/12/2024",
+        title: "Web Innovators Challenge 2024: Ciptakan Website Masa Depan!",
+        field: "Web Design",
+        dateStart: "04/12/2024",
+        dateEnd: "01/01/2025",
         organizer: "Universitas Negeri Surabaya",
         place: "Online",
-        description: "Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum morbi blandit cursus risus.",
+        description: "Bergabunglah dalam Web Innovators Challenge 2024, kompetisi web development yang mengajak para developer dari berbagai tingkat keahlian untuk menunjukkan bakat dan kreativitas mereka! Tantang dirimu untuk menciptakan website inovatif yang tidak hanya menarik secara visual, tetapi juga fungsional dan ramah pengguna.",
         endTime: "2024-12-01T10:10:00",
-        image: "/bg1-carousel.jpg"
+        image: "/bg6-carousel.jpg"
     },
     {
         id: 2,
-        title: "DesignFusion: Innovate the Future of User Experience 2",
-        field: "UI/UX Designer",
-        dateStart: "01/12/2024",
-        dateEnd: "30/12/2024",
+        title: "Cyber Shield Challenge 2025",
+        field: "Cyber Security",
+        dateStart: "02/01/2025",
+        dateEnd: "30/1/2025",
         organizer: "Universitas Negeri Surabaya",
         place: "Online",
-        description: "Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum morbi blandit cursus risus.",
+        description: "Bergabunglah dalam Cyber Shield Challenge 2024, ajang kompetisi cybersecurity yang menguji keterampilan dan kecerdasanmu dalam melindungi sistem dan data! Siapkan dirimu untuk menghadapi tantangan yang menuntut keahlian dalam hacking etis, analisis risiko, dan strategi pertahanan.",
         endTime: "2024-12-01T10:14:00",
-        image: "/bg2-carousel.jpg"
+        image: "/bg4-carousel.jpg"
     },
     {
         id: 3,
-        title: "DesignFusion: Innovate the Future of User Experience 3",
-        field: "UI/UX Designer",
-        dateStart: "01/12/2024",
-        dateEnd: "30/12/2024",
+        title: "Game Development Chalenge 2025",
+        field: "Game Development",
+        dateStart: "01/01/2025",
+        dateEnd: "30/02/2025",
         organizer: "Universitas Negeri Surabaya",
         place: "Online",
-        description: "Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum morbi blandit cursus risus.",
+        description: "Ikuti Game Development Contest 2024 dan tunjukkan bakatmu dalam menciptakan permainan yang menghibur dan inovatif! Kompetisi ini terbuka untuk semua developer, dari pemula hingga profesional, yang ingin mewujudkan ide-ide kreatif mereka dalam bentuk game.",
         endTime: "2024-12-01T10:10:00",
-        image: "/bg3-carousel.jpg"
+        image: "/bg5-carousel.jpg"
+    },
+    {
+        id: 4,
+        title: "Artificial Intelegence Inovation Competition 2025 ",
+        field: "Artificial Intelegence",
+        dateStart: "04/01/2025",
+        dateEnd: "30/03/2025",
+        organizer: "Universitas Negeri Surabaya",
+        place: "Online",
+        description: "Bergabunglah dalam AI Innovation Challenge 2024, kompetisi yang mengajak para pengembang, peneliti, dan pelajar untuk mengeksplorasi potensi kecerdasan buatan! Tunjukkan kemampuanmu dalam menciptakan solusi AI yang inovatif dan berdampak bagi masyarakat.",
+        endTime: "2024-12-01T10:14:00",
+        image: "/bg7-carousel.jpg"
+    },
+    {
+        id: 5,
+        title: "Data Masters Challenge 2024 ",
+        field: "Data Science",
+        dateStart: "03/01/2025",
+        dateEnd: "3/02/2025",
+        organizer: "Universitas Negeri Surabaya",
+        place: "Online",
+        description: " 🎉 Data Masters Challenge 2024 🎉Ungkap Wawasan dari Data! Bergabunglah dalam kompetisi analisis data dan machine learning yang menguji keterampilanmu dalam menggali informasi berharga dari data. Tunjukkan kemampuanmu dan bersaing dengan para ahli di bidang ini! Hadiah Menarik: Sertifikat,Uang Tunai,Kesempatan Kolaborasi dengan Perusahaan Teknologi Terkemuka.Daftar Sekarang dan Buktikan bahwa Kamu adalah Data Master!",
+        endTime: "2024-12-01T10:14:00",
+        image: "/bg8-carousel.jpg"
+    },
+    {
+        id: 6,
+        title: "Artificial Intelegence Inovation Competition 2025 ",
+        field: "Artificial Intelegence",
+        dateStart: "04/01/2025",
+        dateEnd: "30/03/2025",
+        organizer: "Universitas Negeri Surabaya",
+        place: "Online",
+        description: "Bergabunglah dalam Cyber Shield Challenge 2024, ajang kompetisi cybersecurity yang menguji keterampilan dan kecerdasanmu dalam melindungi sistem dan data! Siapkan dirimu untuk menghadapi tantangan yang menuntut keahlian dalam hacking etis, analisis risiko, dan strategi pertahanan.",
+        endTime: "2024-12-01T10:14:00",
+        image: "/bg4-carousel.jpg"
     },
 ];
