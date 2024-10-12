208ccc8e2e956a706ff520f441909b4e990c4b23
Kevin Dzaky <115269741+kevidn@users.noreply.github.com>
2024-10-12 20:05:55 +0700

    Update learningpath.js

--- src/app/home/section/learningpath.js
+++ src/app/home/section/learningpath.js
@@ -5,10 +5,10 @@ function LearningPath() {
         <div className="w-full bg-[#e6e6e6] my-10">
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
                 <div className="text-center text-xl md:text-3xl font-normal text-[#3a3f47]">
-                    Learning Path
+                    Description
                 </div>
                 <p className="text-center text-sm font-light text-[#3a3f47] my-4 w-3/4 md:w-1/2 mx-auto">
-                    The learning path will guide you through studying at the Academy with a curriculum developed in collaboration with leading industry professionals.
+                    The website serves as a multipurpose platform, featuring bootcamps for programmers, meetup events, and competitions for various IT categories.
                 </p>
             </div>
         </div>
