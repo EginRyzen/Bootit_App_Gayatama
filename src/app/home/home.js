6ba8e324ff7a69c131a07ee4872b0c738003698e
EginRyzen <muhammadaliagil@gmail.com>
2024-11-02 21:05:19 +0700

    style:bg

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -23,16 +23,13 @@ function HomePage() {
     return (
         <>
             <main>
-                <div className="fixed flex top-0 -z-10 h-full w-full bg-white">
-                    {/* Warna Ungu */}
+                {/* <div className="fixed flex top-0 -z-10 h-full w-full bg-white">
                     <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
 
-                    {/* Warna Merah */}
                     <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] translate-x-[10%] translate-y-[50%] rounded-full bg-[rgba(244,109,109,0.5)] opacity-50 blur-[80px]"></div>
 
-                    {/* Warna Biru */}
                     <div className="absolute bottom-auto left-0 top-0 h-[500px] w-[500px] translate-x-[10%] translate-y-[20%] rounded-full bg-[rgba(95,164,242,0.5)] opacity-50 blur-[80px]"></div>
-                </div>
+                </div> */}
                 {/* Banner */}
                 <Banner />
 
